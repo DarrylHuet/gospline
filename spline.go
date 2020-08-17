5be070cf06b5220166c5eb0e1d8a4c3dfad14069
DarrylHuet <DarrylHuet@gmail.com>
2020-08-17 16:17:15 -0600

    Update spline.go

--- spline.go
+++ spline.go
@@ -4,7 +4,7 @@ package gospline
 type Spline interface {
 	// At returns interpolated value at x
 	// 0 <= x <= 1
-	At(x float64) float64
+	At(x float64) (float64, [4]float64
 
 	// Range returns interpolated values in [start, end] with step
 	Range(start, end, step float64) []float64
